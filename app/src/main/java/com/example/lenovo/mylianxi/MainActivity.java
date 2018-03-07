dc2866dae4ac865c22212dd5dfd1884645702a64
lhf <358496312@qq.com>
2018-03-07 16:15:13 +0800

    //再次发送

--- app/src/main/java/com/example/lenovo/mylianxi/MainActivity.java
+++ app/src/main/java/com/example/lenovo/mylianxi/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//试一下
+//试一下  哈哈哈哈
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
